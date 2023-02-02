3315b74290635c99068fd0d84c8405b6992b7f54
semenikhin <garry@nextmail.ru>
2023-02-02 23:27:04 +0300

    Library is done

---
+++ src/Homework_12/Author.java
@@ -0,0 +1,22 @@
+package Homework_12;
+
+public class Author {
+    String authorName;
+    String authorSurname;
+
+
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+
+
+    }
+
+    public String getAuthorName() {
+        return this.authorName;
+    }
+
+    public String getAuthorSurname() {
+        return this.authorSurname;
+    }
+}
